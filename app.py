6175d4d3b92590c0587b14142ef9257992b186d8
kosta97416-maker <kosta97416@gmail.com>
2026-04-25 19:35:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # Config IA
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
 @app.route('/')
 def index():
     return render_template('index.html')
