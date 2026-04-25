b8677619dfd0bf2b36b24af2bd74c4aef8a1eaa3
kosta97416-maker <kosta97416@gmail.com>
2026-04-25 21:20:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-mon-repo/
-├── main.py          ← ton code FastAPI
-├── index.html       ← ton interface web
-└── requirements.txt ← tes dépendances
+model = genai.GenerativeModel("gemini-1.5-flash")
